1a3fea68d43e93a6349f8e87de148dc7eb92ebd3
baixiyang <xiyangbai@outlook.com>
2020-09-09 20:37:21 +0800

    update

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -1,4 +1,18 @@
 <template>
-  <el-calendar :range="['2019-03-04', '2019-03-24']">
+  <el-calendar>
+  <!-- 这里使用的是 2.5 slot 语法，对于新项目请使用 2.6 slot 语法-->
+  <template
+    v-slot:dateCell>
+
+    <p>{{slotProps}}</p>
+    <!-- <p :class="data.isSelected ? 'is-selected' : ''">
+      {{ data.day.split('-').slice(1).join('-') }} {{ data.isSelected ? '✔️' : ''}}{{date}}
+    </p> -->
+  </template>
 </el-calendar>
 </template>
+<style>
+  .is-selected {
+    color: #1989FA;
+  }
+</style>
